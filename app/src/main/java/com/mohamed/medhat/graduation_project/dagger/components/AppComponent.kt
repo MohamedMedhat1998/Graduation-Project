78524ea1695d59ec6f9ca2652c1d3583060b0644
MohamedMedhat <abomed7at55@gmail.com>
2021-01-11 12:33:00 +0200

    -Added a network layer to the application

--- app/src/main/java/com/mohamed/medhat/graduation_project/dagger/components/AppComponent.kt
+++ app/src/main/java/com/mohamed/medhat/graduation_project/dagger/components/AppComponent.kt
@@ -2,6 +2,7 @@ package com.mohamed.medhat.graduation_project.dagger.components
 
 import android.content.Context
 import com.mohamed.medhat.graduation_project.dagger.modules.AppSubcomponentsModule
+import com.mohamed.medhat.graduation_project.dagger.modules.NetworkModule
 import dagger.BindsInstance
 import dagger.Component
 import javax.inject.Singleton
@@ -10,7 +11,7 @@ import javax.inject.Singleton
  * A dagger component for the app lifecycle.
  */
 @Singleton
-@Component(modules = [AppSubcomponentsModule::class])
+@Component(modules = [AppSubcomponentsModule::class, NetworkModule::class])
 interface AppComponent {
 
     @Component.Factory
---
+++ app/src/main/java/com/mohamed/medhat/graduation_project/networking/WebApi.kt
@@ -0,0 +1,8 @@
+package com.mohamed.medhat.graduation_project.networking
+
+import com.mohamed.medhat.graduation_project.repository.Api
+
+/**
+ * Contains all the endpoints from the web service. This class returns real data from the remote RESTFUL web service.
+ */
+interface WebApi : Api
