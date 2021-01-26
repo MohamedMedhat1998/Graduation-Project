527c23520998cc77d877c9c511eea5da6e436d05
MohamedMedhat <abomed7at55@gmail.com>
2021-01-26 20:14:43 +0200

    -Added the required classes to monitor the network state

--- app/src/main/java/com/mohamed/medhat/graduation_project/dagger/DaggerApplication.kt
+++ app/src/main/java/com/mohamed/medhat/graduation_project/dagger/DaggerApplication.kt
@@ -1,8 +1,13 @@
 package com.mohamed.medhat.graduation_project.dagger
 
 import android.app.Application
+import android.net.ConnectivityManager
+import android.net.Network
+import android.net.NetworkRequest
+import android.widget.Toast
 import com.mohamed.medhat.graduation_project.dagger.components.AppComponent
 import com.mohamed.medhat.graduation_project.dagger.components.DaggerAppComponent
+import com.mohamed.medhat.graduation_project.networking.NetworkState
 
 /**
  * A dagger application.
@@ -15,4 +20,26 @@ class DaggerApplication : Application() {
         DaggerAppComponent.factory().create(applicationContext)
     }
 
+    override fun onCreate() {
+        super.onCreate()
+        val builder: NetworkRequest.Builder = NetworkRequest.Builder()
+        val connectivityManager = (getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager)
+        connectivityManager.registerNetworkCallback(
+            builder.build(),
+            object : ConnectivityManager.NetworkCallback() {
+                override fun onAvailable(network: Network) {
+                    super.onAvailable(network)
+                    NetworkState.isConnected = true
+                    Toast.makeText(this@DaggerApplication, "Connected", Toast.LENGTH_SHORT)
+                        .show()
+                }
+
+                override fun onLost(network: Network) {
+                    super.onLost(network)
+                    NetworkState.isConnected = false
+                    Toast.makeText(this@DaggerApplication, "Disconnected", Toast.LENGTH_SHORT)
+                        .show()
+                }
+            })
+    }
 }
