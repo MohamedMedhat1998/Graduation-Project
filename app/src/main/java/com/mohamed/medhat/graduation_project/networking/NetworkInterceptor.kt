0a618e8333d2de6eb6eff8f893f2908567fd2b29
MohamedMedhat <abomed7at55@gmail.com>
2021-02-07 13:17:35 +0200

    -Updated the interceptor

--- app/src/main/java/com/mohamed/medhat/graduation_project/networking/NetworkInterceptor.kt
+++ app/src/main/java/com/mohamed/medhat/graduation_project/networking/NetworkInterceptor.kt
@@ -2,6 +2,7 @@ package com.mohamed.medhat.graduation_project.networking
 
 import com.mohamed.medhat.graduation_project.utils.managers.TokenManager
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 import javax.inject.Inject
 
@@ -9,18 +10,41 @@ import javax.inject.Inject
  * A class that is responsible for intercepting all the requests to and the responses from the RESTFUL api.
  */
 class NetworkInterceptor @Inject constructor(val tokenManager: TokenManager) : Interceptor {
+
+    // TODO update this list whenever a new endpoint is added to [WebApi] interface
+    private val nonAuthEndpoints = listOf(
+        "${BASE_URL}Accounts/Mentor/Register",
+        "${BASE_URL}Accounts/Login"
+    )
+
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request = chain.request()
-        val token = tokenManager.getToken()
-        if (token.isNotEmpty()) {
-            val newRequest =
-                request.newBuilder().addHeader("Authorization", "Bearer $token")
-                    .build()
-            return chain.proceed(newRequest)
+        val originalRequest = chain.request()
+        return if (requiresAuth(originalRequest)) {
+            // For requests that REQUIRE authentication, proceed with a NEW REQUEST and add the
+            // TOKEN to the header.
+            val newRequest = originalRequest.newBuilder()
+                .addHeader("Authorization", "Bearer ${tokenManager.getToken()}")
+                .build()
+            chain.proceed(newRequest)
+        } else {
+            // For requests that don't require authentication, proceed with the original request
+            // without adding any token to the header.
+            chain.proceed(originalRequest)
         }
-        //Log.d("Request URL", request.url.toString())
-        //Log.d("Response Code", response.code.toString())
-        //Log.d("Response", response.peekBody(1024).string())
-        return chain.proceed(request)
+    }
+
+    /**
+     * A helper function that checks whether a request requires authentication using a token or not.
+     * @param request the [Request] object under the check.
+     * @return `true` if the request requires authentication, `false` otherwise.
+     */
+    private fun requiresAuth(request: Request): Boolean {
+        val url = request.url.toString()
+        nonAuthEndpoints.forEach {
+            if (url == it) {
+                return false
+            }
+        }
+        return true
     }
 }
