465e5adc65a20fc9a2c5ab786bb8aca122ea28a5
MohamedMedhat <abomed7at55@gmail.com>
2021-05-20 17:41:38 +0200

    -Finished SplashActivity UI (but without the logo)

--- app/src/main/java/com/mohamed/medhat/sanad/ui/splash_activity/SplashActivity.kt
+++ app/src/main/java/com/mohamed/medhat/sanad/ui/splash_activity/SplashActivity.kt
@@ -1,6 +1,7 @@
 package com.mohamed.medhat.sanad.ui.splash_activity
 
 import android.animation.Animator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.view.View
 import androidx.lifecycle.ViewModelProviders
@@ -8,6 +9,7 @@ import com.mohamed.medhat.sanad.R
 import com.mohamed.medhat.sanad.ui.base.BaseActivity
 import com.mohamed.medhat.sanad.ui.base.network_state_awareness.NetworkStateUnaware
 import com.mohamed.medhat.sanad.ui.custom_listeners.AnimationEndListener
+import kotlinx.android.synthetic.main.activity_splash.*
 import javax.inject.Inject
 
 /**
@@ -48,6 +50,101 @@ class SplashActivity : BaseActivity(), SplashView {
             .rotation(rotation)
             .setDuration(duration)
             .setListener(animationListener)
+            .start()
+    }
+
+    override fun scaleAppLogo(onComplete: () -> Unit) {
+        iv_app_logo.animate()
+            .scaleX(1.45f)
+            .scaleY(1.45f)
+            .setDuration(800)
+            .start()
+        val animator = iv_app_logo2.animate()
+            .scaleX(1.45f)
+            .scaleY(1.45f)
+            .setDuration(800)
+        animator.setListener(object : AnimationEndListener {
+            override fun onAnimationEnd(animation: Animator?) {
+                animator.setListener(null)
+                onComplete.invoke()
+            }
+        })
+        animator.start()
+    }
+
+    override fun scaleCircle1(onComplete: () -> Unit) {
+        val animator = iv_splash_circle.animate()
+            .scaleX(1f)
+            .scaleY(1f)
+            .setDuration(600)
+        animator.setListener(object : AnimationEndListener {
+            override fun onAnimationEnd(animation: Animator?) {
+                animator.setListener(null)
+                onComplete.invoke()
+            }
+        })
+        animator.start()
+    }
+
+    override fun hideFirstLogo() {
+        val hide = ValueAnimator.ofFloat(1f, 0f)
+        hide.addUpdateListener {
+            iv_app_logo.alpha = it.animatedValue as Float
+        }
+        hide.duration = 600
+        hide.start()
+    }
+
+    override fun showSecondLogo() {
+        val show = ValueAnimator.ofFloat(0f, 1f)
+        show.addUpdateListener {
+            iv_app_logo2.alpha = it.animatedValue as Float
+        }
+        show.duration = 600
+        show.start()
+    }
+
+    override fun scaleCircle2(onComplete: () -> Unit) {
+        val animator = iv_splash_circle.animate()
+            .scaleXBy(1.25f)
+            .scaleYBy(1.25f)
+            .setDuration(600)
+        animator.setListener(object : AnimationEndListener {
+            override fun onAnimationEnd(animation: Animator?) {
+                animator.setListener(null)
+                onComplete.invoke()
+            }
+        })
+        animator.start()
+    }
+
+    override fun scaleAppLogo2() {
+        iv_app_logo2.animate()
+            .scaleYBy(2f)
+            .scaleXBy(2f)
+            .setDuration(300)
+            .start()
+    }
+
+    override fun scaleCircle3(onComplete: () -> Unit) {
+        val animator = iv_splash_circle.animate()
+            .scaleXBy(2f)
+            .scaleYBy(3f)
+            .setDuration(600)
+        animator.setListener(object : AnimationEndListener {
+            override fun onAnimationEnd(animation: Animator?) {
+                animator.setListener(null)
+                onComplete.invoke()
+            }
+        })
+        animator.start()
+    }
+
+    override fun squeezeAppLogo() {
+        iv_app_logo2.animate()
+            .scaleX(0f)
+            .scaleY(0f)
+            .setDuration(600)
             .start()
     }
 
